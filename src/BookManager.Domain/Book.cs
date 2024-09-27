b7cb2b0ecb951feebe6cd9ddeb83017fe33eef27
Kevin Wüstner <kevin.wuestner@xappido.com>
2024-09-27 20:36:19 +0200

    feat(domain): add domain

---
+++ src/BookManager.Domain/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace BookManager.Domain;
+
+public class Book
+{
+    public Guid Id { get; set; }
+    
+    public string Title { get; set; }
+    
+    public string Description { get; set; }
+    
+    public int PageCount { get; set; }
+    
+    public Guid AuthorId { get; set; }
+    
+    public Author Author { get; set; }
+    
+    public IEnumerable<Genre> Genres { get; set; }
+}
